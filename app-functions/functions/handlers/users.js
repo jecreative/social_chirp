50993c2bed3f47fe89a8cbe2a3a77b3f3c35ed08
Jordan Esguerra <creative@jordanesguerra.com>
2020-10-02 13:57:34 -0700

    Fixed issue with .update() in users.js by adding a check in reduceUserDetails in the util/validators.js file. The patch checks for empty strings in case the user wants to remove details from their profile completely.

--- app-functions/functions/handlers/users.js
+++ app-functions/functions/handlers/users.js
@@ -1,15 +1,15 @@
-const { admin, db } = require('../util/admin');
+const { admin, db } = require('../util/admin')
 
-const config = require('../util/config');
+const config = require('../util/config')
 
-const firebase = require('firebase');
-firebase.initializeApp(config);
+const firebase = require('firebase')
+firebase.initializeApp(config)
 
 const {
   validateSignupData,
   validateLoginData,
   reduceUserDetails,
-} = require('../util/validators');
+} = require('../util/validators')
 
 // Sign users up
 exports.signup = (req, res) => {
@@ -18,120 +18,120 @@ exports.signup = (req, res) => {
     password: req.body.password,
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle,
-  };
+  }
 
-  const { valid, errors } = validateSignupData(newUser);
+  const { valid, errors } = validateSignupData(newUser)
 
-  if (!valid) return res.status(400).json(errors);
+  if (!valid) return res.status(400).json(errors)
 
-  const noImg = 'no-img.png';
+  const noImg = 'no-img.png'
 
-  let token, userId;
+  let token, userId
   db.doc(`/users/${newUser.handle}`)
     .get()
     .then((doc) => {
       if (doc.exists) {
-        return res.status(400).json({ handle: 'this handle is already taken' });
+        return res.status(400).json({ handle: 'this handle is already taken' })
       } else {
         return firebase
           .auth()
-          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+          .createUserWithEmailAndPassword(newUser.email, newUser.password)
       }
     })
     .then((data) => {
-      userId = data.user.uid;
-      return data.user.getIdToken();
+      userId = data.user.uid
+      return data.user.getIdToken()
     })
     .then((idToken) => {
-      token = idToken;
+      token = idToken
       const userCredentials = {
         handle: newUser.handle,
         email: newUser.email,
         createdAt: new Date().toISOString(),
         imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
         userId,
-      };
-      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+      }
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials)
     })
     .then(() => {
-      return res.status(201).json({ token });
+      return res.status(201).json({ token })
     })
     .catch((err) => {
-      console.error(err);
+      console.error(err)
       if (err.code === 'auth/email-already-in-use') {
-        return res.status(400).json({ email: 'Email is already is use' });
+        return res.status(400).json({ email: 'Email is already is use' })
       } else {
         return res
           .status(500)
-          .json({ general: 'Something went wrong, please try again' });
+          .json({ general: 'Something went wrong, please try again' })
       }
-    });
-};
+    })
+}
 
 // Login users
 exports.login = (req, res) => {
   const user = {
     email: req.body.email,
     password: req.body.password,
-  };
+  }
 
-  const { valid, errors } = validateLoginData(user);
+  const { valid, errors } = validateLoginData(user)
 
-  if (!valid) return res.status(400).json(errors);
+  if (!valid) return res.status(400).json(errors)
 
   firebase
     .auth()
     .signInWithEmailAndPassword(user.email, user.password)
     .then((data) => {
-      return data.user.getIdToken();
+      return data.user.getIdToken()
     })
     .then((token) => {
-      return res.json({ token });
+      return res.json({ token })
     })
     .catch((err) => {
-      console.error(err);
+      console.error(err)
       // auth/wrong-password
       // auth/user-not-user
       return res
         .status(403)
-        .json({ general: 'Wrong credentials, please try again' });
-    });
-};
+        .json({ general: 'Wrong credentials, please try again' })
+    })
+}
 
 // Add user details
 exports.addUserDetails = (req, res) => {
-  let userDetails = reduceUserDetails(req.body);
+  let userDetails = reduceUserDetails(req.body)
 
   db.doc(`/users/${req.user.handle}`)
     .update(userDetails)
     .then(() => {
-      return res.json({ message: 'Details added successfully' });
+      return res.json({ message: 'Details added successfully' })
     })
     .catch((err) => {
-      console.error(err);
-      return res.status(500).json({ error: err.code });
-    });
-};
+      console.error(err)
+      return res.status(500).json({ error: err.code })
+    })
+}
 
 // Get any user's details
 exports.getUserDetails = (req, res) => {
-  let userData = {};
+  let userData = {}
   db.doc(`/users/${req.params.handle}`)
     .get()
     .then((doc) => {
       if (doc.exists) {
-        userData.user = doc.data();
+        userData.user = doc.data()
         return db
           .collection('chirps')
           .where('userHandle', '==', req.params.handle)
           .orderBy('createdAt', 'desc')
-          .get();
+          .get()
       } else {
-        return res.status(404).json({ error: 'User not found' });
+        return res.status(404).json({ error: 'User not found' })
       }
     })
     .then((data) => {
-      userData.chirps = [];
+      userData.chirps = []
       data.forEach((doc) => {
         userData.chirps.push({
           body: doc.data().body,
@@ -141,44 +141,44 @@ exports.getUserDetails = (req, res) => {
           likeCount: doc.data().likeCount,
           commentCount: doc.data().commentCount,
           chirpId: doc.id,
-        });
-      });
-      return res.json(userData);
+        })
+      })
+      return res.json(userData)
     })
     .catch((err) => {
-      console.error(err);
-      return res.status(500).json({ error: err.code });
-    });
-};
+      console.error(err)
+      return res.status(500).json({ error: err.code })
+    })
+}
 
 // Get own user details
 exports.getAuthenticatedUser = (req, res) => {
-  let userData = {};
+  let userData = {}
   db.doc(`/users/${req.user.handle}`)
     .get()
     .then((doc) => {
       if (doc.exists) {
-        userData.credentials = doc.data();
+        userData.credentials = doc.data()
         return db
           .collection('likes')
           .where('userHandle', '==', req.user.handle)
-          .get();
+          .get()
       }
     })
     .then((data) => {
-      userData.likes = [];
+      userData.likes = []
       data.forEach((doc) => {
-        userData.likes.push(doc.data());
-      });
+        userData.likes.push(doc.data())
+      })
       return db
         .collection('notifications')
         .where('recipient', '==', req.user.handle)
         .orderBy('createdAt', 'desc')
         .limit(10)
-        .get();
+        .get()
     })
     .then((data) => {
-      userData.notifications = [];
+      userData.notifications = []
       data.forEach((doc) => {
         userData.notifications.push({
           recipient: doc.data().recipient,
@@ -188,42 +188,42 @@ exports.getAuthenticatedUser = (req, res) => {
           type: doc.data().type,
           read: doc.data().read,
           notificationId: doc.id,
-        });
-      });
-      return res.json(userData);
+        })
+      })
+      return res.json(userData)
     })
     .catch((err) => {
-      console.error(err);
-      return res.status(500).json({ error: err.code });
-    });
-};
+      console.error(err)
+      return res.status(500).json({ error: err.code })
+    })
+}
 
 // Upload user profile image
 exports.uploadImage = (req, res) => {
-  const BusBoy = require('busboy');
-  const path = require('path');
-  const os = require('os');
-  const fs = require('fs');
+  const BusBoy = require('busboy')
+  const path = require('path')
+  const os = require('os')
+  const fs = require('fs')
 
-  const busboy = new BusBoy({ headers: req.headers });
+  const busboy = new BusBoy({ headers: req.headers })
 
-  let imageFileName;
-  let imageToBeUploaded = {};
+  let imageFileName
+  let imageToBeUploaded = {}
 
   busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
     if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
-      return res.status(400).json({ error: 'Wrong file type submitted' });
+      return res.status(400).json({ error: 'Wrong file type submitted' })
     }
     // my.image.png
-    const imageExtension = filename.split('.')[filename.split('.').length - 1];
+    const imageExtension = filename.split('.')[filename.split('.').length - 1]
     // 352452346532.png
     imageFileName = `${Math.round(
       Math.random() * 100000000000
-    )}.${imageExtension}`;
-    const filepath = path.join(os.tmpdir(), imageFileName);
-    imageToBeUploaded = { filepath, mimetype };
-    file.pipe(fs.createWriteStream(filepath));
-  });
+    )}.${imageExtension}`
+    const filepath = path.join(os.tmpdir(), imageFileName)
+    imageToBeUploaded = { filepath, mimetype }
+    file.pipe(fs.createWriteStream(filepath))
+  })
   busboy.on('finish', () => {
     admin
       .storage()
@@ -237,33 +237,33 @@ exports.uploadImage = (req, res) => {
         },
       })
       .then(() => {
-        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
-        return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`
+        return db.doc(`/users/${req.user.handle}`).update({ imageUrl })
       })
       .then(() => {
-        return res.json({ message: 'Image uploaded successfully' });
+        return res.json({ message: 'Image uploaded successfully' })
       })
       .catch((err) => {
-        console.error(err);
-        return res.status(500).json({ error: err.code });
-      });
-  });
-  busboy.end(req.rawBody);
-};
+        console.error(err)
+        return res.status(500).json({ error: err.code })
+      })
+  })
+  busboy.end(req.rawBody)
+}
 
 exports.markNotificationsRead = (req, res) => {
-  let batch = db.batch();
+  let batch = db.batch()
   req.body.forEach((notificationId) => {
-    const notification = db.doc(`/notifications/${notificationId}`);
-    batch.update(notification, { read: true });
-  });
+    const notification = db.doc(`/notifications/${notificationId}`)
+    batch.update(notification, { read: true })
+  })
   batch
     .commit()
     .then(() => {
-      return res.json({ message: 'Notifications marked read' });
+      return res.json({ message: 'Notifications marked read' })
     })
     .catch((err) => {
-      console.error(err);
-      return res.status(500).json({ error: err.code });
-    });
-};
+      console.error(err)
+      return res.status(500).json({ error: err.code })
+    })
+}
